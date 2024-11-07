a976e650e53de81a315aea1c0d834a8311eeda41
quratulainasim <quratulainasim1234@gmail.com>
2024-11-07 23:24:57 +0500

    Add files via upload

---
+++ milestone1-2/script.js
@@ -0,0 +1,5 @@
+var skills = document.getElementById("skills");
+var skil_btn = document.getElementById("skil_btn");
+skil_btn === null || skil_btn === void 0 ? void 0 : skil_btn.addEventListener("click", function () {
+    skills.classList.toggle("hide");
+});
---
+++ milestone1-2/script.ts
@@ -0,0 +1,7 @@
+let skills =document.getElementById ("skills")
+let skil_btn =document.getElementById ("skil_btn")
+
+skil_btn?.addEventListener("click", ()=>{
+  skills!.classList.toggle("hide")  
+});
+
