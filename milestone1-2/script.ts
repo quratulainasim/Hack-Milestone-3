0fd6a92719eac8c42362bd9ffb6a3785a4e0ed45
quratulainasim <quratulainasim1234@gmail.com>
2024-11-08 18:59:27 +0500

    milestone3

--- milestone1-2/script.ts
+++
@@ -1,7 +0,0 @@
-let skills =document.getElementById ("skills")
-let skil_btn =document.getElementById ("skil_btn")
-
-skil_btn?.addEventListener("click", ()=>{
-  skills!.classList.toggle("hide")  
-});
-
